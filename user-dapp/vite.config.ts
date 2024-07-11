248e2c01cfa41f350d3bf48acbe1bafef7bc69d8
sweetim <hosweetim@gmail.com>
2024-07-11 21:52:29 +0900

    added pwa

--- user-dapp/vite.config.ts
+++ user-dapp/vite.config.ts
@@ -4,10 +4,54 @@ import {
   URL,
 } from "url"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ react() ],
+  plugins: [
+    react(),
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: [ "**/*.{js,css,html,ico,png,svg}" ],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "meishi",
+        short_name: "meishi",
+        description: "decentralized business card exchange dapp",
+        theme_color: "#18181B",
+        scope: "/",
+        start_url: "/",
+        icons: [
+          {
+            "src": "pwa-64x64.png",
+            "sizes": "64x64",
+            "type": "image/png",
+          },
+          {
+            "src": "pwa-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+          },
+          {
+            "src": "pwa-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+          },
+          {
+            "src": "maskable-icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable",
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
